1ac367b2fd2b128ecff5617e4ea5bc583adff9b9
Christoph Hackenberger <chackenberger@student.tgm.ac.at>
2014-03-20 19:36:42 +0100

    Elevator now uses bump sensors for height regulation

--- src/at/shufflebots/modules/Elevator.java
+++ src/at/shufflebots/modules/Elevator.java
@@ -1,23 +1,26 @@
 package at.shufflebots.modules;
 
 import linkjvm.motors.Motor;
+import linkjvm.sensors.digital.DigitalSensor;
 
 public class Elevator {
 
 	private Motor top;
 	private Motor bottom;
+	private DigitalSensor topSensor;
+	private DigitalSensor bottomSensor;
 	
-	public Elevator(int top_motor_port, int bottom_motor_port) {
+	public Elevator(int top_motor_port, int bottom_motor_port, int top_sensor_port, int bottom_sensor_port) {
 		top = new Motor(top_motor_port);
 		bottom = new Motor(bottom_motor_port);
+		topSensor = new DigitalSensor(top_sensor_port);
+		bottomSensor = new DigitalSensor(bottom_sensor_port);
 	}
 	
 	public void up() {
 		top.run(-100);
 		bottom.run(7);
-	}
-	
-	public void stop() {
+		while(!this.topSensor.getValue());
 		top.off();
 		bottom.off();
 	}
@@ -25,5 +28,8 @@ public class Elevator {
 	public void down() {
 		top.run(7);
 		bottom.run(-100);
+		while(!this.bottomSensor.getValue());
+		top.off();
+		bottom.off();
 	}
 }
--- src/at/shufflebots/modules/testing/ElevatorTest.java
+++ src/at/shufflebots/modules/testing/ElevatorTest.java
@@ -3,7 +3,6 @@ package at.shufflebots.modules.testing;
 import at.shufflebots.modules.Elevator;
 import linkjvm.Botball;
 import linkjvm.sensors.buttons.AButton;
-import linkjvm.sensors.buttons.BButton;
 import linkjvm.sensors.buttons.CButton;
 import linkjvm.sensors.buttons.SideButton;
 
@@ -12,21 +11,16 @@ public class ElevatorTest {
 	public static void main(String[] args) {
 		AButton a = new AButton();
 		a.setText("Up");
-		BButton b = new BButton();
-		b.setText("Stop");
 		CButton c = new CButton();
 		c.setText("Down");
 		SideButton sb = new SideButton();
-		Elevator lift = new Elevator(1, 0);
+		Elevator lift = new Elevator(1, 0, 8, 9);
 		while(!sb.isPressed()) {
 			if(a.isPressed())
 				lift.up();
-			if(b.isPressed())
-				lift.stop();
 			if(c.isPressed())
 				lift.down();
 			Botball.msleep(100);
 		}
-		lift.stop();
 	}
 }
